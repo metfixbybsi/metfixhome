a8ba2665ca3ea2427f8a8a1377715ed520e74bc4
Mia Kotalik <mia.kotalik@gmail.com>
2026-08-17 11:47:35 -0700

    Use Sanity Viewer token so course pages load on GitHub Pages

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -4,6 +4,7 @@ interface ImportMetaEnv {
   readonly VITE_SANITY_PROJECT_ID?: string;
   readonly VITE_SANITY_DATASET?: string;
   readonly VITE_SANITY_API_VERSION?: string;
+  readonly VITE_SANITY_API_READ_TOKEN?: string;
   readonly VITE_FRONTEND_FORGE_API_KEY?: string;
   readonly VITE_FRONTEND_FORGE_API_URL?: string;
 }
